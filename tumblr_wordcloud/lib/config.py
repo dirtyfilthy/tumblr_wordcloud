efb44472b96f19b082a683d41427fecb386c2a20
Zoom <alhazred@alhazreds-MacBook-Pro.local>
2021-07-07 05:11:58 +1000

    fix

--- tumblr_wordcloud/lib/config.py
+++ tumblr_wordcloud/lib/config.py
@@ -1,4 +1,4 @@
 import os
-APIKEY = "sPFsQ9eKPuderEBtGHjtc40ljYUMKCgDlx3IdiGv1vJ1Ziwjp0"
+APIKEY = "YOURAPIKEY"
 MAX_POSTS = 400
 DATA_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "static", "generated"))
